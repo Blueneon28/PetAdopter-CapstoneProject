fb964bc483c50cbe40c6ce3649733e67d761bc06
Adyatma Fulvian <viantok28@gmail.com>
2022-08-11 23:32:39 +0700

    Add Layout

---
+++ components/Layout/index.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Header from "../Header";
+
+export default function Layout({ headTitle, headDesc, children }) {
+  return (
+    <div className="w-full min-h-screen bg-white dark:bg-black text-black dark:text-white font-Poppins">
+      <Head>
+        <title>{headTitle}</title>
+        <meta name="description" content={headDesc} />
+        <link rel="icon" href="/logo-petdopter.png" />
+      </Head>
+
+      <Header />
+      <main className="w-full h-full">{children}</main>
+    </div>
+  );
+}
