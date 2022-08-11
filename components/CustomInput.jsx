c9dec10901f7dc644f004632cd429953f28e2f0a
faisolrr <faisolrizkiromdon@gmail.com>
2022-08-11 21:42:52 +0700

    create input combobox components

--- components/CustomInput.jsx
+++ components/CustomInput.jsx
@@ -11,4 +11,21 @@ function CustomInput({ id, type, placeholder, value, onChange }) {
   );
 }
 
-export { CustomInput };
+function CustomInputComboBox({ id, title, op1, op2, op3 }) {
+  return (
+    <select
+      id={id}
+      defaultValue="none"
+      className="w-72 p-1 rounded-lg font-light text-sm italic border-2 border-primary font-Poppins bg-white"
+    >
+      <option value="none" disabled>
+        {title}
+      </option>
+      <option value={op1}>{op1}</option>
+      <option value={op2}>{op2}</option>
+      <option value={op3}>{op3}</option>
+    </select>
+  );
+}
+
+export { CustomInput, CustomInputComboBox };
