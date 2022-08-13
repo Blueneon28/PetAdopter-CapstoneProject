bc640cb320b39312f558e387671d86b9abe0e497
faisolrr <faisolrizkiromdon@gmail.com>
2022-08-13 23:06:09 +0700

    fix slicing

--- pages/meetings/add.jsx
+++ pages/meetings/add.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 import Layout from "../../components/Layout";
@@ -8,7 +8,7 @@ import { SmallButton } from "../../components/CustomButton";
 export default function AddMeeting() {
   const router = useRouter();
   const [date, setDate] = useState("");
-  const [time, settime] = useState("");
+  const [time, setTime] = useState("");
   const [loading, setLoading] = useState(false);
   const [disabled, setDisabled] = useState(true);
 
@@ -62,17 +62,21 @@ export default function AddMeeting() {
                   id="inputDate"
                   type="text"
                   placeholder="Date"
-                  // onChange={(e) => setDate(e.target.value)}
+                  onChange={(e) => setDate(e.target.value)}
                 />
                 <CustomInput
                   id="inputTime"
                   type="text"
                   placeholder="Time"
-                  // onChange={(e) => setPhoneTime(e.target.value)}
+                  onChange={(e) => setTime(e.target.value)}
                 />
               </div>
               <div className="pt-20 space-x-2 flex flex-cols-2 justify-center">
-                <SmallButton label="Add" loading={loading || disabled} />
+                <SmallButton
+                  label="Add"
+                  loading={loading || disabled}
+                  type="submit"
+                />
                 <SmallButton
                   href="/meetings"
                   label="cancel"
