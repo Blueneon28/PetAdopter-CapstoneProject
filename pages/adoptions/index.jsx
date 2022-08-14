89d2e0b815244e1343bd73b5d9854dcd3b0115f7
Adyatma Fulvian <viantok28@gmail.com>
2022-08-14 18:55:56 +0700

    Add Adoptions page

--- pages/adoptions/index.jsx
+++ pages/adoptions/index.jsx
@@ -3,7 +3,7 @@ import { getCookie, deleteCookie } from "cookies-next";
 
 import Layout from "../../components/Layout";
 import TitlePage from "../../components/TitlePage";
-import PetCard from "../../components/PetCard";
+import AdoptionCard from "../../components/AdoptionCard";
 
 export async function getServerSideProps({ req, res }) {
   const token = getCookie("token", { req, res });
@@ -22,7 +22,7 @@ export async function getServerSideProps({ req, res }) {
     },
   };
   const response = await fetch(
-    "https://virtserver.swaggerhub.com/Capstone-tim1/PetAdopter-tim1/1.0.0/mypets",
+    "https://virtserver.swaggerhub.com/Capstone-tim1/PetAdopter-tim1/1.0.0/myadoption",
     requestOptions
   );
   const data = await response.json();
@@ -41,24 +41,24 @@ export async function getServerSideProps({ req, res }) {
   }
 }
 
-export default function MyPets({ data, token }) {
-  const [pets, setPets] = useState(data);
+export default function MyAdoptions({ data, token }) {
+  const [adoptions, setAdoptions] = useState(data);
   const [loading, setLoading] = useState(false);
 
   return (
-    <Layout headTitle="My Pets" headDesc="Welcome to petdopter!">
+    <Layout headTitle="Adoption History" headDesc="Welcome to adoptiondopter!">
       <div className="p-4 md:px-12 lg:px-24">
-        <TitlePage page="My Pets" />
+        <TitlePage page="Adoption History" />
         <div className="my-4 grid md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8 lg:gap-12">
-          {pets.map((pet) => (
-            <PetCard
-              key={pet.petid}
-              id={pet.petid}
-              name={pet.petname}
-              image={pet.petphoto}
-              gender={pet.gender}
-              city={pet.city}
-              age={pet.age}
+          {adoptions.map((adoption) => (
+            <AdoptionCard
+              key={adoption.adoptionid}
+              petid={adoption.petid}
+              petname={adoption.petname}
+              petimage={adoption.petphoto}
+              ownername={adoption.ownername}
+              city={adoption.owneraddress}
+              status={adoption.status}
             />
           ))}
         </div>
