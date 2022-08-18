333e99bd17184e9f6675b209f2eba417c63638c3
faisolrr <faisolrizkiromdon@gmail.com>
2022-08-19 03:28:34 +0700

    add google oauth integration

---
+++ pages/auth/[id].jsx
@@ -0,0 +1,18 @@
+import { setCookie } from "cookies-next";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+export default function Redirect() {
+  const router = useRouter();
+  useEffect(() => {
+    const keysvalue = location.search;
+    const result = new URLSearchParams(keysvalue);
+    const token = result.get("token");
+    const role = result.get("role");
+
+    if (token) {
+      setCookie("token", token);
+      role === "user" ? router.replace("/") : router.replace("/admin");
+    }
+  }, []);
+  return;
+}
