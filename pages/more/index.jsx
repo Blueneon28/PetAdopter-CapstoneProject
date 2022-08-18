cb6d3ef0d7388e99e2b8bde2dc67dc19980fc2aa
Adyatma Fulvian <viantok28@gmail.com>
2022-08-19 03:09:22 +0700

    Add integration in more page

--- pages/more/index.jsx
+++ pages/more/index.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { getCookie, deleteCookie } from "cookies-next";
 
 import {
   MdPets,
@@ -12,7 +15,52 @@ import {
 import Layout from "../../components/Layout";
 import TitlePage from "../../components/TitlePage";
 
-export default function MoreMenu() {
+export async function getServerSideProps({ req, res }) {
+  const token = getCookie("token", { req, res });
+  if (!token) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/auth/login",
+      },
+    };
+  }
+  const requestOptions = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const response = await fetch(
+    "https://golangprojectku.site/users",
+    requestOptions
+  );
+  const data = await response.json();
+  if (response.status === 200) {
+    return {
+      props: { code: data.code, data: data.data, message: data.message, token },
+    };
+  } else {
+    deleteCookie("token");
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/auth/login",
+      },
+    };
+  }
+}
+
+export default function MoreMenu({ data }) {
+  const router = useRouter();
+
+  const [user, setUser] = useState(data);
+
+  const handleLogout = (e) => {
+    e.preventDefault;
+    deleteCookie("token");
+    router.push("/auth/login");
+  };
   return (
     <Layout headTitle="Petdopter" headDesc="Welcome to petdopter!">
       <div className="p-4 md:px-12 lg:px-24">
@@ -21,8 +69,16 @@ export default function MoreMenu() {
           <Link href="/profile">
             <a className="flex justify-between items-center my-4 font-bold">
               <div className="flex items-center">
+                <Image
+                  src={user.photoprofile}
+                  alt={user.fullname}
+                  width={300}
+                  height={300}
+                  layout={"responsive"}
+                  className="rounded-full"
+                />
                 <MdOutlineAccountCircle size={70} />
-                <h1 className="mx-4 text-xl">User</h1>
+                <h1 className="mx-4 text-xl">{user.fullname}</h1>
               </div>
               <MdChevronRight size={30} />
             </a>
@@ -43,9 +99,13 @@ export default function MoreMenu() {
                 </a>
               </Link>
             </div>
-            <div className="w-full">
+            <button
+              type="button"
+              className="w-full text-left"
+              onClick={(e) => handleLogout(e)}
+            >
               <MdOutlineLogout size={30} className="inline mr-2" /> Logout
-            </div>
+            </button>
           </div>
         </div>
       </div>
