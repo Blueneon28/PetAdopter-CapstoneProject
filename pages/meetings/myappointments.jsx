692e5519f8b22ec0a14eb814a18827441a82785d
faisolrr <faisolrizkiromdon@gmail.com>
2022-08-19 18:42:29 +0700

    change condition for code 404

--- pages/meetings/myappointments.jsx
+++ pages/meetings/myappointments.jsx
@@ -23,7 +23,6 @@ export async function getServerSideProps({ req, res }) {
   };
   const response = await fetch(
     "https://golangprojectku.site/meetings",
-    // "https://virtserver.swaggerhub.com/Capstone-tim1/PetAdopter-tim1/1.0.0/meetings",
     requestOptions
   );
   const data = await response.json();
@@ -32,42 +31,63 @@ export async function getServerSideProps({ req, res }) {
       props: { code: data.code, data: data.data, message: data.message, token },
     };
   } else {
-    deleteCookie("token");
-    return {
-      redirect: {
-        permanent: false,
-        destination: "/",
-      },
-    };
+    if (response.status === 404) {
+      return {
+        props: { data: "" },
+      };
+    } else {
+      deleteCookie("token");
+      return {
+        redirect: {
+          permanent: false,
+          destination: "/auth/login",
+        },
+      };
+    }
   }
 }
 
 export default function MyAppointments({ data, token }) {
   const [dataMeetings] = useState(data);
-
-  return (
-    <Layout
-      headTitle="My Appointments"
-      headDesc="All My Meetings Appointment List"
-    >
-      <div className="p-4 md:px-12 lg:px-24">
-        <TitlePage page="My Appointments" />
-        <div className="py-4 md:py-6 font-Poppins grid justify-center">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-            {dataMeetings.map((data) => (
-              <MyAppointmentCard
-                key={data.meetingid}
-                id={data.meetingid}
-                date={data.date}
-                time={data.time}
-                place={data.owneraddress}
-                petname={data.petname}
-                seekername={data.seekername}
-              />
-            ))}
+  if (!data) {
+    return (
+      <Layout
+        headTitle="My Appointments"
+        headDesc="All My Meetings Appointment List"
+      >
+        <div className="p-4 md:px-12 lg:px-24">
+          <TitlePage page="My Appointments" />
+          <div className="py-4 md:py-6 font-Poppins grid justify-center">
+            no appointments yet
           </div>
         </div>
-      </div>
-    </Layout>
-  );
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout
+        headTitle="My Appointments"
+        headDesc="All My Meetings Appointment List"
+      >
+        <div className="p-4 md:px-12 lg:px-24">
+          <TitlePage page="My Appointments" />
+          <div className="py-4 md:py-6 font-Poppins grid justify-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+              {dataMeetings.map((data) => (
+                <MyAppointmentCard
+                  key={data.meetingid}
+                  id={data.meetingid}
+                  date={data.date}
+                  time={data.time}
+                  place={data.owneraddress}
+                  petname={data.petname}
+                  seekername={data.seekername}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
 }
--- pages/meetings/myinvitations.jsx
+++ pages/meetings/myinvitations.jsx
@@ -22,9 +22,8 @@ export async function getServerSideProps({ req, res }) {
     },
   };
   const response = await fetch(
-    "https://golangprojectku.site/mymeetings"
-    // "https://virtserver.swaggerhub.com/Capstone-tim1/PetAdopter-tim1/1.0.0/mymeetings",
-    // requestOptions
+    "https://golangprojectku.site/mymeetings",
+    requestOptions
   );
   const data = await response.json();
   if (response.status === 200) {
@@ -32,41 +31,64 @@ export async function getServerSideProps({ req, res }) {
       props: { code: data.code, data: data.data, message: data.message, token },
     };
   } else {
-    deleteCookie("token");
-    return {
-      redirect: {
-        permanent: false,
-        destination: "/",
-      },
-    };
+    if (response.status === 404) {
+      return {
+        props: { data: "" },
+      };
+    } else {
+      deleteCookie("token");
+      return {
+        redirect: {
+          permanent: false,
+          destination: "/auth/login",
+        },
+      };
+    }
   }
 }
 
 export default function MyInvitations({ data }) {
   const [dataMeetings] = useState(data);
-  return (
-    <Layout
-      headTitle="My Invitations"
-      headDesc="All My Meetings Invitation List"
-    >
-      <div className="p-4 md:px-12 lg:px-24">
-        <TitlePage page="My Invitations" />
-        <div className="py-4 md:py-6 font-Poppins grid justify-center">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-            {dataMeetings.map((data) => (
-              <MyInvitationCard
-                key={data.meetingid}
-                id={data.meetingid}
-                date={data.date}
-                time={data.time}
-                place={data.owneraddress}
-                petname={data.petname}
-                ownername={data.ownername}
-              />
-            ))}
+
+  if (!data) {
+    return (
+      <Layout
+        headTitle="My Invitations"
+        headDesc="All My Meetings Appointment List"
+      >
+        <div className="p-4 md:px-12 lg:px-24">
+          <TitlePage page="My Invitations" />
+          <div className="py-4 md:py-6 font-Poppins grid justify-center">
+            no invitation yet
           </div>
         </div>
-      </div>
-    </Layout>
-  );
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout
+        headTitle="My Invitations"
+        headDesc="All My Meetings Invitation List"
+      >
+        <div className="p-4 md:px-12 lg:px-24">
+          <TitlePage page="My Invitations" />
+          <div className="py-4 md:py-6 font-Poppins grid justify-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+              {dataMeetings.map((data) => (
+                <MyInvitationCard
+                  key={data.meetingid}
+                  id={data.meetingid}
+                  date={data.date}
+                  time={data.time}
+                  place={data.owneraddress}
+                  petname={data.petname}
+                  ownername={data.ownername}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
 }
